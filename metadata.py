7b4bd2f42a0407b97032ae2e9269f5b66df700e6
Andrew Lu <andrewmlu3@gmail.com>
2023-06-19 19:44:15 -0400

    add college newspapers list

--- metadata.py
+++ metadata.py
@@ -1,101 +1,133 @@
+import os
+import re
 import json
+from datetime import datetime
 
 FILE_PATH = "/mnt/data01/pipeline_backup/pipeline_egress/"
 
-def load_data_from_minibatch(minibatch_path):
-    # read from .ocr_text.json
-    with open(minibatch_path + "ocr_text.json", "r") as f:
-        metadata = json.load(f)
-    return metadata
 
-def list_all_subdirs(dir_path):
-    import os
-    subdirs = [name for name in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, name))]
-    return subdirs
+class ManifestCollection:
+    """
+    Class representing a collection of multiple manifests
+    """
+    def __init__(self, manifest_names, filename):
+        """
+        Initialize a ManifestCollection with a list of manifest names and the name of a file containing newspaper names
+        """
+        with open(filename, "r") as f:
+            newspapers = f.read().splitlines()
+        self.manifests = [Manifest(name, newspapers) for name in manifest_names]
 
-def load_metadata_from_manifest(manifest_name, newspaper_set):
-    # read each mini_batch from each directory labeled mini_batch_xxx and pass into load_metadata_from_minibatch
-    # list all directories in manifest_path
-    subdirs = list_all_subdirs(FILE_PATH + manifest_name)
-    # load metadata from each mini_batch
-    metadata_by_newspaper = initialize_metadata_by_newspaper(newspaper_set)
-    for i, subdir in enumerate(subdirs):
-        if (i < 20):
-            # print(subdir)
-            minibatch_data = load_data_from_minibatch(FILE_PATH + manifest_name + "/" + subdir + "/")
-            minibatch_keys = get_keys_from_minibatch(minibatch_data)
-            minibatch_metadata = get_metadata_from_keys(minibatch_keys)
-            selected_mini_batch_metadata = select_metadata_by_newspaper(minibatch_metadata, newspaper_set)
-            append_metadata_by_newspaper(metadata_by_newspaper, selected_mini_batch_metadata)
-    return metadata_by_newspaper
+    @classmethod
+    def from_directory(cls, dir_path, filename):
+        """
+        Class method to create a ManifestCollection object based on all manifests found in a directory
+        """
+        manifest_names = [name for name in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, name))]
+        return cls(manifest_names, filename)
 
-def load_newspaper_set(filename):
-    # load newspaper list as set
-    with open(filename, "r") as f:
-        newspaper_list = f.read().splitlines()
-    return set(newspaper_list)
+    def get_all_metadata(self):
+        """
+        Get all metadata from all manifests in the collection
+        """
+        all_metadata = {}
+        for manifest in self.manifests:
+            for newspaper, metadata in manifest.metadata.items():
+                if newspaper not in all_metadata:
+                    all_metadata[newspaper] = metadata
+                else:
+                    all_metadata[newspaper].extend(metadata)
+        return all_metadata
 
-def select_metadata_by_newspaper(metadata, newspaper_set):
-    selected_metadata = {}
-    for d in metadata:
-        if d['Newspaper'] in newspaper_set:
-            if d['Newspaper'] not in selected_metadata:
-                selected_metadata[d['Newspaper']] = [d]
-            else:
-                selected_metadata[d['Newspaper']].append(d)
-    return selected_metadata
+class Manifest:
+    """
+    Class representing a Manifest which contains multiple minibatches
+    """
+    def __init__(self, name, newspapers):
+        """
+        Initialize Manifest with its name and newspapers (set of newspaper names)
+        """
+        self.name = name
+        self.newspapers = set(newspapers)
+        self.metadata = self.load_metadata()
+    @staticmethod
+    def from_file(manifest_name, filename):
+        """
+        Static method to create a Manifest object from a file
+        """
+        with open(filename, "r") as f:
+            newspapers = f.read().splitlines()
+        return Manifest(manifest_name, newspapers)
+    def load_metadata(self):
+        """
+        Load metadata from all minibatches in the manifest for all newspapers in the set
+        """
+        metadata = {newspaper: [] for newspaper in self.newspapers}
+        subdirs = self.list_subdirs()
+        for i, subdir in enumerate(subdirs):
+            # if i < 20:  # Limiting to the first 20 directories for now
+            minibatch = Minibatch(self.name, subdir)
+            metadata = minibatch.append_metadata(metadata)
+        return metadata
+    def list_subdirs(self):
+        """
+        List all subdirectories in the manifest directory
+        """
+        path = os.path.join(FILE_PATH, self.name)
+        subdirs = [name for name in os.listdir(path) if os.path.isdir(os.path.join(path, name))]
+        return subdirs
 
-def get_keys_from_minibatch(minibatch):
-    # get the filenames from metadata
-    keys = list(minibatch.keys())
-    return keys
-
-def extract_metadata_from_filename(filename):
-    import re
-    from datetime import datetime
-    # Regular expression pattern
-    pattern = r"(\d+)-(.+?)-(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)-(\d+)-(\d+)-p-(\d+)\.jpg"
-    # Match the pattern
-    match = re.match(pattern, filename)
-    dict = {}
-    if match:
-        # Parse date
-        date_str = f"{match.group(5)}-{match.group(3)}-{match.group(4)}"
-        date = datetime.strptime(date_str, "%Y-%b-%d")
-        # Create a dictionary
-        dict = {
-            "ID": match.group(1),
-            "Newspaper": match.group(2).replace("-", " "),
-            "Date": date.strftime("%Y-%m-%d"),  # formatted as YYYY-MM-DD
-            "Pg": match.group(6)
+class Minibatch:
+    """
+    Class representing a Minibatch which is part of a Manifest
+    """
+    def __init__(self, manifest_name, name):
+        """
+        Initialize Minibatch with its manifest_name and its own name
+        """
+        self.manifest_name = manifest_name
+        self.name = name
+        self.data = self.load_data()
+    def load_data(self):
+        """
+        Load minibatch data from the ocr_text.json file
+        """
+        path = os.path.join(FILE_PATH, self.manifest_name, self.name, "ocr_text.json")
+        with open(path, "r") as f:
+            data = json.load(f)
+        return data
+    def append_metadata(self, metadata):
+        """
+        Extract metadata from the filenames in the minibatch data and append it to the provided metadata dictionary
+        """
+        for key, _ in self.data.items():
+            item_metadata = self.extract_metadata_from_filename(key)
+            newspaper = item_metadata.get('Newspaper')
+            if newspaper in metadata:
+                metadata[newspaper].append(item_metadata)
+        return metadata
+    @staticmethod
+    def extract_metadata_from_filename(filename):
+        """
+        Extract metadata from a filename
+        """
+        pattern = r"(\d+)-(.+?)-(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)-(\d+)-(\d+)-p-(\d+)\.jpg"
+        match = re.match(pattern, filename)
+        dict = {}
+        if match:
+            date_str = f"{match.group(5)}-{match.group(3)}-{match.group(4)}"
+            date = datetime.strptime(date_str, "%Y-%b-%d")
+            dict = {
+                "ID": match.group(1),
+                "Newspaper": match.group(2).replace("-", " "),
+                "Date": date.strftime("%Y-%m-%d"),
+                "Pg": match.group(6)
         }
-    return dict
+        return dict
 
-def get_metadata_from_keys(keys):
-    metadata = [extract_metadata_from_filename(key) for key in keys]
-    return metadata
 
-def initialize_metadata_by_newspaper(newspaper_set):
-    metadata = {}
-    for newspaper in newspaper_set:
-        metadata[newspaper] = []
-    return metadata
-
-def append_metadata_by_newspaper(metadata_by_newspaper, metadata):
-    for key, value in metadata.items():
-        if key in metadata_by_newspaper:
-            metadata_by_newspaper[key].extend(value)
-
-def get_metadata_metadata(metadata_by_newspaper):
-    metadata = {}
-    for key, value in metadata_by_newspaper.items():
-        metadata[key] = len(value)
-        # metadata[key] = len([d for d in value if d['Pg'] == '1'])
-    return metadata
-
-# test_keys = get_keys_from_minibatch(test_metadata[0])
-# print(len(test_keys))
-# extract_metadata_from_filename(test_keys[0])
-test_newsp_set = set(["cambridge daily jeffersonian"])
-test_metadata = load_metadata_from_manifest("all1973_size50_julyManifest", test_newsp_set)
-test_newsp_metadata = select_metadata_by_newspaper(test_metadata, test_newsp_set)
+# Usage:
+if __name__ == "__main__":
+    collection = ManifestCollection.from_directory(FILE_PATH, "newspapers.txt")
+    all_metadata = collection.get_all_metadata()
+    print(all_metadata)
